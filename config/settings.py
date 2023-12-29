9b05c5a3375f518c18d39a4a7eb5759284157a46
Tachmyrat <annageldiyewic02@gmail.com>
2023-12-29 19:51:33 +0500

    adsasd

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', "*"]
+ALLOWED_HOSTS = ['127.0.0.1', "http://tacmyrat02.pythonanywhere.com/"]
 
 
 # Application definition
