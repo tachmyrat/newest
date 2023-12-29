e720212438a61c9c11bd10377c37a1ce29fd9b55
Tachmyrat <annageldiyewic02@gmail.com>
2023-12-29 19:26:39 +0500

    newest

--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from environ import Env
 from .secret import SECRET_KEY
-env = Env()
-env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
