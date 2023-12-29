bce6fa81a63b9e48bed92ae425fd51e8cdf3dbed
Tachmyrat <annageldiyewic02@gmail.com>
2023-12-29 19:32:30 +0500

    asd

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG",default=False)
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
