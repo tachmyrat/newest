dba009a23848ac990a29ada7e4014cb4bac020dc
Tachmyrat <annageldiyewic02@gmail.com>
2023-12-29 18:59:41 +0500

    b

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG",default=False)
 
-ALLOWED_HOSTS = ['https://www.pythonanywhere.com/','127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
